fbd0eeafe33e29b4dd1527dff5cc352226907506
Stefan de Konink <stefan@konink.de>
2016-12-18 20:02:23 +0100

    Fixes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_codename.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_lenny3.mk
 
--- omni_lenny3.mk
+++ omni_lenny3.mk
@@ -4,9 +4,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk) # If y
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
 
-# Inherit device configuration
-$(call inherit-product, device/wiko/lenny3/device.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_lenny3
 PRODUCT_DEVICE := lenny3
