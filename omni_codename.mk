4176c0f9bd29d872d84a2ad121a5fd217fc2b2a9
Stefan de Konink <stefan@konink.de>
2016-12-18 17:42:37 +0100

    Fill in the device names

--- omni_codename.mk
+++ omni_codename.mk
@@ -8,14 +8,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are bu
 $(call inherit-product, device/vendor/codename/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_codename
-PRODUCT_DEVICE := codename
-PRODUCT_BRAND := vendor
-PRODUCT_MODEL := codename
-PRODUCT_MANUFACTURER := vendor
+PRODUCT_NAME := omni_lenny3
+PRODUCT_DEVICE := lenny3
+PRODUCT_BRAND := wiko
+PRODUCT_MODEL := lenny3
+PRODUCT_MANUFACTURER := wiko
 
 # If needed to overide these props
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="" \
     PRIVATE_BUILD_DESC=""
-    
+
